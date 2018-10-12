be6b5d383effb6907dd65d108f8fc46f883bcab6
Vladislav Kovun <vkovun@phoenixit.ru>
2018-10-12 13:55:52 +0300

    Changes spawning mechanism.

--- dungeon_initializer.go
+++ dungeon_initializer.go
@@ -7,37 +7,38 @@ import (
 )
 
 func (dung *dungeon) initialize_level() { //crap of course
-	dung.player = p_createPlayer(1, 1)
+	dung.MakeMapFromGenerated()
+
+	dung.spawnPlayerAtRandomPosition()
+
 	dung.pawns = make([]*p_pawn, 0)
 
-	dung.MakeMapFromGenerated()
 	dung.init_placeItemsAndEnemies()
 }
 
 func (dung *dungeon) init_placeItemsAndEnemies() {
-	for i := 0; i < 15; i++ {
-		dung.spawnPawnAtRandomPosition("unwilling")
-		dung.spawnPawnAtRandomPosition("zombie soldier")
-		dung.spawnPawnAtRandomPosition("zombie sergeant")
-		dung.spawnPawnAtRandomPosition("imp")
-	}
-	for i := 0; i < 5; i++ {
-		dung.spawnItemAtRandomPosition("stimpack")
-		dung.spawnItemAtRandomPosition("clip")
-		dung.spawnItemAtRandomPosition("cell")
-		dung.spawnItemAtRandomPosition("shells")
-		dung.spawnItemAtRandomPosition("ammunition crate")
-		dung.spawnItemAtRandomPosition("small medikit")
-	}
-	dung.spawnItemAtRandomPosition("soulsphere")
-	dung.spawnItemAtRandomPosition("chaingun")
-	dung.spawnItemAtRandomPosition("heavy pistol")
-	dung.spawnItemAtRandomPosition("shotgun")
-	dung.spawnItemAtRandomPosition("super shotgun")
-	dung.spawnItemAtRandomPosition("assault rifle")
-	dung.spawnItemAtRandomPosition("Pancor Jackhammer")
-	dung.spawnItemAtRandomPosition("bolt-action rifle")
-	dung.spawnItemAtRandomPosition("gauss rifle")
+	dung.spawnPawnAtRandomPosition("unwilling", 15)
+	dung.spawnPawnAtRandomPosition("zombie soldier", 10)
+	dung.spawnPawnAtRandomPosition("zombie sergeant", 10)
+	dung.spawnPawnAtRandomPosition("imp", 7)
+
+	dung.spawnItemAtRandomPosition("stimpack", 10)
+	dung.spawnItemAtRandomPosition("clip", 5)
+	dung.spawnItemAtRandomPosition("cell", 5)
+	dung.spawnItemAtRandomPosition("shells", 10)
+	dung.spawnItemAtRandomPosition("ammunition crate", 3)
+
+	dung.spawnItemAtRandomPosition("small medikit", 5)
+	dung.spawnItemAtRandomPosition("soulsphere", 2)
+
+	dung.spawnItemAtRandomPosition("chaingun", 1)
+	dung.spawnItemAtRandomPosition("heavy pistol", 3)
+	dung.spawnItemAtRandomPosition("shotgun", 2)
+	dung.spawnItemAtRandomPosition("super shotgun", 1)
+	dung.spawnItemAtRandomPosition("assault rifle", 2)
+	dung.spawnItemAtRandomPosition("Pancor Jackhammer", 1)
+	dung.spawnItemAtRandomPosition("bolt-action rifle", 3)
+	dung.spawnItemAtRandomPosition("gauss rifle", 1)
 }
 
 func (dung *dungeon) MakeMapFromGenerated() {
@@ -69,22 +70,37 @@ func (dung *dungeon) MakeMapFromGenerated() {
 	}
 }
 
-func (dung *dungeon) spawnPawnAtRandomPosition(name string) {
+
+func (dung *dungeon) spawnPlayerAtRandomPosition() {
 	for tries := 0; tries < 1000; tries++ {
 		x, y := routines.Random(levelsizex), routines.Random(levelsizey)
-		if dung.isTilePassableAndNotOccupied(x, y) {
-			dung.pawns = append(dung.pawns, p_createPawn(name, x, y))
-			return
+		if dung.isTilePassable(x, y) {
+			dung.player = p_createPlayer(x, y)
+			break
 		}
 	}
 }
 
-func (dung *dungeon) spawnItemAtRandomPosition(name string) {
-	for tries := 0; tries < 1000; tries++ {
-		x, y := routines.Random(levelsizex), routines.Random(levelsizey)
-		if dung.isTilePassableAndNotOccupied(x, y) {
-			dung.items = append(dung.items, i_createItem(name, x, y))
-			return
+func (dung *dungeon) spawnPawnAtRandomPosition(name string, count int) { //spawns outside of player's FOV
+	for num := 0; num < count; num++ {
+		for tries := 0; tries < 1000; tries++ {
+			x, y := routines.Random(levelsizex), routines.Random(levelsizey)
+			if dung.isTilePassableAndNotOccupied(x, y) && !dung.canPawnSeeCoords(dung.player, x, y) {
+				dung.pawns = append(dung.pawns, p_createPawn(name, x, y))
+				break
+			}
+		}
+	}
+}
+
+func (dung *dungeon) spawnItemAtRandomPosition(name string, count int) {
+	for num := 0; num < count; num++ {
+		for tries := 0; tries < 1000; tries++ {
+			x, y := routines.Random(levelsizex), routines.Random(levelsizey)
+			if dung.isTilePassableAndNotOccupied(x, y) {
+				dung.items = append(dung.items, i_createItem(name, x, y))
+				break
+			}
 		}
 	}
 }
