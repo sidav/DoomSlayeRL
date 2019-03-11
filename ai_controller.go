861062fd7cd63fb200000408eae12de0eb9a8c83
Vladislav Kovun <vkovun@phoenixit.ru>
2019-03-11 14:37:14 +0300

    Makes the AI use pathfinding.

--- ai_controller.go
+++ ai_controller.go
@@ -16,13 +16,14 @@ type (
 )
 
 const (
-	AI_SHOOT_CHANCE                   = 15
-	AI_STEP_BACK_CHANCE               = 80
-	AI_STEP_BACK_THRESHOLD            = 3 * 3
-	AI_SILENT              ai_aiState = 0
-	AI_ROAMING             ai_aiState = 1
-	AI_ENGAGING            ai_aiState = 2
-	AI_STAGGERED           ai_aiState = 3
+	AI_SHOOT_CHANCE                        = 15
+	AI_STEP_BACK_CHANCE                    = 80
+	AI_STEP_BACK_THRESHOLD                 = 3 * 3
+	AI_DISTANCE_TO_STOP_CHASING            = 12
+	AI_SILENT                   ai_aiState = iota
+	AI_ROAMING                  ai_aiState = iota
+	AI_ENGAGING                 ai_aiState = iota
+	AI_STAGGERED                ai_aiState = iota
 )
 
 func ai_decideMove(monster *p_pawn, dung *dungeon) {
@@ -31,7 +32,11 @@ func ai_decideMove(monster *p_pawn, dung *dungeon) {
 	switch aiData.state {
 	case AI_ENGAGING:
 		ex, ey := aiData.currentTarget.getCoords()
-		vx, vy := ai_getVectorToTarget(monster, ex, ey)
+		path := dung.getPathFromTo(monster.x, monster.y, ex, ey)
+		vx, vy := path.GetNextStepVector()
+		if vx == 0 && vy == 0 {
+			vx, vy = ai_getVectorToTarget(monster, ex, ey)
+		}
 		if monster.canShoot() {
 			// if the distance is less than threshold, then step back. Maybe.
 			if getSqDistance(monster.x, monster.y, ex, ey) < AI_STEP_BACK_THRESHOLD && routines.RandomPercent() < AI_STEP_BACK_CHANCE {
@@ -67,7 +72,9 @@ func ai_reactToSurroundings(monster *p_pawn, dung *dungeon) { //change state if 
 	} else {
 		switch aiData.state {
 		case AI_ENGAGING:
-			aiData.state = AI_ROAMING
+			if getSqDistance(monster.x, monster.y, ex, ey) > AI_DISTANCE_TO_STOP_CHASING * AI_DISTANCE_TO_STOP_CHASING {
+				aiData.state = AI_ROAMING
+			}
 			return
 		}
 	}
